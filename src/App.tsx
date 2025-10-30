b7923515ccda9f1af669c4ef129d350511312748
[dyad] <592672849@qq.com>
2025-10-30 21:29:01 +0800

    [dyad] 创建直接显示的秒表计时器组件 - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import Timer from "./pages/Timer";
+import Timer from "./pages/Index";
 
 const queryClient = new QueryClient();
 
@@ -14,14 +11,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/timer" element={<Timer />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Timer />
     </TooltipProvider>
   </QueryClientProvider>
 );
